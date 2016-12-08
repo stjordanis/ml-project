bea04574e29ce59f677ecfae80607d327eaf0751
Caroline Morganti <morganti@mit.edu>
2016-12-08 02:22:35 -0500

    cleaning up code

--- neuralnet.py
+++ neuralnet.py
@@ -15,38 +15,23 @@ import load_lfw
 ##############################################################################
 #lfw_people = fetch_lfw_people(min_faces_per_person=50, resize=0.4)
 lfw_people = load_lfw.load_pairs(type='funneled')
-#print(lfw_people)
 fold0 = lfw_people[0]
-#print("fold0 = ", fold0)
-# Find the shapes of the images.
-#num_images, h, w = lfw_people.images.shape
-#print(fold0)
+
 # Extract the input to the neural network and the number of features.
-#X = lfw_people.data
-#print(X)
+
 target_names = np.asarray([(fold0[x][0][0], fold0[x][1][0]) for x in range(len(fold0))])
-#print(target_names)
-print(target_names.shape)
-#print([fold0[x][y][0] for x in range(len(fold0)) for y in range(0,2)])
+
 target_nums = np.asarray([int(x==y) for (x,y) in target_names])
-#target_nums = np.reshape(target_nums, (600,1))
-print("target_nums.shape = ", target_nums.shape)
+
 data = []
 for x in range(len(fold0)):
 	data.append((np.ravel(fold0[x][0][2]),np.ravel(fold0[x][1][2])))
 X = np.asarray(data)
-print("fold0[x][0][2].shape = " , fold0[x][0][2].shape)
-print("fold0[x][1][2].shape = " , fold0[x][1][2].shape)
+
 images =np.asarray([ np.concatenate((fold0[x][0][2], fold0[x][1][2]), axis=0) for x in range(len(fold0))])
 images = np.reshape(images, (600, 500, 250, 1))
-print("images.shape =", images.shape)
-#X = np.asarray([fold0[x][y][2] for x in range(len(fold0)) for y in range(0,2)])
-#X = np.ravel(X)
-# (1200, 250, 250)
+
 num_features = X.shape[2] #should be 62500
-print("num_features = ", num_features)
-print(X.shape)
-print(images.shape) 
 num_pairs = images.shape[0]
 h = 250 # TODO hard-coded
 w = 250
@@ -57,11 +42,8 @@ w = 250
 num_classes = 1 # since we are classifying whether the people are the same or different
 print("num_classes = ", num_classes)
 
-# TODO logic here to account for pairs 
 
 t = np.zeros([num_pairs, num_classes])
-#print(np.arange(num_pairs))
-#t[np.arange(num_pairs), target_nums] = 1
 t[np.arange(num_pairs), 0] = target_nums
 
 print("Total dataset size:")
@@ -71,26 +53,12 @@ print("#identities: %d" % num_classes)
 # DOES X_train and X_test contain concatenated images??? (500, 250)
 # Create training and test sets. 
 X_train, X_test, t_train, t_test = train_test_split(images, t, test_size=0.25, random_state=0)
-print("t_train.shape = ", t_train.shape)
-print("X_train.shape = ", X_train.shape)
-#print("t_train = ", t_train)
-#print("t_test = ", t_test)
-print("X_test.shape = ", X_test.shape)
-print("t_test.shape = ", t_test.shape)
-#print("X_test = ", X_test)
-#print("t_test = ", t_test)
-'''
-t_train.shape =  (450, 2)
-X_train.shape =  (450, 2, 62500)
-X_test.shape =  (150, 2, 62500)
-t_test.shape =  (150, 2)
-'''
 
 ###############################################################################
 #   NEURAL NETWORK CONSTANTS                                                  #
 ###############################################################################
 # HYPERPARAMETERS
-NUM_TRAINING_STEPS = 50
+NUM_TRAINING_STEPS = 500
 
 batch_size = 30
 learning_rate = 1e-4
@@ -110,7 +78,7 @@ layer2_pool_filter_size = 2
 layer2_pool_stride = 1
 
 
-fully_connected_nodes = 30 #100
+fully_connected_nodes = 100
 
 
 def weight_variable(shape):
@@ -164,12 +132,9 @@ print(conv2_h.get_shape())
 
 # Pooling layer.
 pool2_h = max_pool(conv2_h, w=2, h=2, sw=1, sh=1)
-#h_pool2 = max_pool(h_conv2, w=layer2_pool_filter_size, h=layer2_pool_filter_size, sw=layer2_pool_stride, sh=layer2_pool_stride)
 _, w, h, d = pool2_h.get_shape().as_list()
-#_, pool2w, pool2h, pool2d = h_pool2.get_shape().as_list()
-#units = pool2w * pool2h * pool2d
 pool2_h_flat = tf.reshape(pool2_h, [-1, w*h*d])
-#h_pool2_flat = tf.reshape(h_pool2, [-1, units])
+
 
 print("pool2_h_flat.get_shape()")
 print(pool2_h_flat.get_shape())
@@ -185,8 +150,6 @@ print(fc1_h.get_shape())
 # Output layer.
 out_w = weight_variable([fully_connected_nodes, num_classes])
 out_b = bias_variable([num_classes])
-# one of these
-#output_layer = tf.nn.l2_normalize(tf.matmul(fc1_h, out_w) + out_b, 1)
 output_layer = tf.sigmoid(tf.matmul(fc1_h, out_w) + out_b)
 print_output = tf.Print(output_layer, [output_layer], message='Output values : ')
 
@@ -195,16 +158,12 @@ print(output_layer.get_shape())
 
 # Layer storing the actual target values.
 target_layer = tf.placeholder(tf.float32, [None, num_classes])
-print_target = tf.Print(target_layer, [target_layer], message='Target values : ')
-# TODO -- load this?
-print("target_layer.get_shape()")
-print(target_layer.get_shape())
+#print_target = tf.Print(target_layer, [target_layer], message='Target values : ')
 
 # The loss function.
 cross_entropy = tf.reduce_mean(tf.square(output_layer - target_layer)) # tf.nn.sigmoid_cross_entropy_with_logits(output_layer, target_layer) # tf.reduce_mean(
 print_loss = tf.Print(cross_entropy, [cross_entropy], message='Loss values : ')
 
-# tf.square(output_layer - target_layer) #
 # The training step.
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
@@ -213,16 +172,7 @@ train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 #   CALCULATE ACCURACY                                                        #
 ###############################################################################
 def accuracy(X, t):
-    #correct_prediction = tf.equal(tf.argmax(output_layer,1), tf.argmax(target_layer,1))
-    print("ACCURACY")
-    print("X.shape = ", X.shape) 
-    print("t.shape = ", t.shape)
-   # ('X.shape = ', (150, 500, 250, 1))
-   # ('t.shape = ', (150, 1))
-    print("X = ", X)
-    print("t = ", t)
     correct_prediction = tf.equal(tf.round(tf.to_float(output_layer), tf.to_float(target_layer)))
-    print("correct_prediction = ", correct_prediction)
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     acc = sess.run(accuracy, feed_dict={target_layer:t, input_layer:X})
     return acc
@@ -241,21 +191,14 @@ for i in range(NUM_TRAINING_STEPS):
 
     X_batch = X_train[idxs[:batch_size]]
     t_batch = t_train[idxs[:batch_size]]
-    #print("X_batch.shape = ", X_batch.shape)
-    #print("X_train.shape = ", X_train.shape)
-    #print("t_batch.shape = ", t_batch.shape)
-    #print("t_train.shape = ", t_train.shape)
 
-
-    _, loss_val, out, target, loss = sess.run([train_step, cross_entropy, print_output, print_target, print_loss], feed_dict={target_layer:t_batch, input_layer:X_batch})
+    _, loss, out = sess.run([train_step, print_output, print_loss], feed_dict={target_layer:t_batch, input_layer:X_batch})
     
     if i % 10 == 9:
         train_time = time() - t0
         print('Batch %d: %f seconds' % (i, train_time))
         t0=time()
-       # print("loss_val = ", loss_val)
-       # print("out = ", out)
-       # print("target = ", target)
+
  
 
 ###############################################################################
@@ -264,21 +207,15 @@ for i in range(NUM_TRAINING_STEPS):
 #print('TEST ACCURACY: ')
 #print(accuracy(X_test, t_test))
 
-# TODO
-# Single sigmoid output
-# anchor + match + non-match
+
 ###############################################################################
 #   RUN THE NEURAL NETWORK                                                    #
 ###############################################################################
 print("Predicting people's names on the test set")
 test = tf.round(output_layer)
 y_pred = sess.run(test, feed_dict={input_layer:X_test})
-print("y_pred = ", y_pred)
-#t_pred = np.argmax(t_test, 1)
 t_pred = t_test
-print("t_pred = ", t_pred)
 
-#print(classification_report(t_pred, y_pred, target_names=target_names))
+
 print(classification_report(t_pred, y_pred))
-#print(confusion_matrix(t_pred, y_pred, labels=range(num_classes)))
 print(confusion_matrix(t_pred, y_pred))
