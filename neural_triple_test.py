efbaeb7d5a641aa5af342f99a623831d5abf363b
Ubuntu <ubuntu@hi.csail.mit.edu>
2016-12-09 13:31:38 -0500

    data pca

--- neural_triple_test.py
+++ neural_triple_test.py
@@ -5,12 +5,15 @@ train, test = neural.generate_model(20, 48, 100, iterations=4001, batch_size=3, 
 print(load_lfw.run_test(1, train, test, 'funneled', .4, True, file='sets48x100.npy', crop=(120,250), ids=True))
 
 # 253 + 179
-#  10, 24x50, 20001 iterations, batch_size 1, color False, consider 2, 5000 for lr
+#  10?, 24x50, 20001 iterations, batch_size 1, color False, consider 2, 5000 for lr
 
 # 254 + 194
-#  10, 36x75, 5001 iterations, batch_size 5, color False, consider 2, 5000 for lr
+#  10?, 36x75, 5001 iterations, batch_size 5, color False, consider 2, 5000 for lr
 
 # 244 + 183
-# 10, 24x50, 15001 iterations, batch_size 1, color True, consider 2, 5000 for lr
+# 10?, 24x50, 15001 iterations, batch_size 1, color True, consider 2, 5000 for lr
 # 178 + 220
 # 7001 iterations
+
+# 235+182
+# 20, 48x100, 4001 iterations, batch_size 3, color=True, consider=2, crop=120x250
